c1d91f11ee61b8baaa5da030bdadfb708fc61fe0
Sean <uanex711@gmail.com>
2022-12-13 20:34:38 +0800

    update mysql password

--- src/main/java/com/groupproduct/controller/GroupproductServlet.java
+++ src/main/java/com/groupproduct/controller/GroupproductServlet.java
@@ -18,7 +18,7 @@ import javax.servlet.http.Part;
 import com.groupproduct.model.GroupproductService;
 import com.groupproduct.model.GroupproductVO;
 @WebServlet("/front-end/groupproduct/Groupproduct.do")
-@MultipartConfig
+
 public class GroupproductServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
