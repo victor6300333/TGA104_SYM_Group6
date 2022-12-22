7c6576d3894c738b29ebe795185dc6865371b414
gamerslink <100190888+gamerslink@users.noreply.github.com>
2022-12-22 14:08:02 +0800

    AdJDBCDAO - 改用注入取得DataSource

--- src/main/java/com/group6/tibame104/ad/model/AdJDBCDAO.java
+++ src/main/java/com/group6/tibame104/ad/model/AdJDBCDAO.java
@@ -10,10 +10,12 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class AdJDBCDAO implements AdDAO_interface {
+	@Autowired
 	private DataSource dataSource;
 
 	private static final String INSERT_STMT = "INSERT INTO advertise (administratorID, groupBuyID, adTitle, adType, adDescribe, adStartDate, adEndDate, adPhoto) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
